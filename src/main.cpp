61bda3449b1630cef77e1433c0ae5151c0dc9ea5
Boris Vasiliev <bvasiliev@icloud.com>
2024-03-07 22:29:50 +0200

    add functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,40 @@
 #include <Arduino.h>
-int NUM = 0;
-int time = 0;
 
-void right(int time)
-{
-    int NUM = (200);
-    analogWrite(6, 201);
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
-    analogWrite(5, 252);
-    digitalWrite(7, LOW);
-    digitalWrite(11, HIGH);
-    delay(time);
-}
-void left(int time)
-{
+// Right Motor
+int ENA = 6;
+int IN1 = 10;
+int IN2 = 9;
 
-    int NUM = (150);
-    analogWrite(6, 188);
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
-    analogWrite(5, 86);
-    digitalWrite(7, LOW);
-    digitalWrite(11, HIGH);
-    delay(time);
-}
+// Left Motor
+int ENB = 5;
+int IN3 = 11;
+int IN4 = 7;
 
-void forward(int time)
-{
-    int NUM = (255);
-    analogWrite(6, 254);
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
-    analogWrite(5, 255);
-    digitalWrite(7, LOW);
-    digitalWrite(11, HIGH);
-    delay(time);
-}
+// PD Control
+int error, previous_error;
+int kd, kp;
+int IRsensors[5];
+int position;
+const int base =2000;
 
-void stop(int time)
-{
-    int NUM = (150);
-    analogWrite(6, 0);
-    analogWrite(5, 0);
-    delay(time);
-}
+void calculateError();
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(6, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(7, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
 }
 void loop()
 {
+}
 
-    forward(3000);
-    right(2000);
-    left(2000);
-    stop(3000);
+void calculateError()
+{
+    position = (0*IRsensors[0]+1000*IRsensors[1]+2000*IRsensors[2]+3000*IRsensors[4]+4000*IRsensors[4]);
+    position /= (IRsensors[0]+IRsensors[1]+IRsensors[2]+IRsensors[3]+IRsensors[4]);
 }
